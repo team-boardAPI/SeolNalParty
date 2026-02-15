00eac0d408693aad62beac8d0d409e157503bd14
gumba <gumba6740@gmail.com>
2026-02-15 19:44:05 +0900

    feat: 데이터베이스
    * feat: 데이터베이스 작업 시작
    
    - 디비 작업 시작
    
    * feat: DB 초기 설정
    
    - core/database.py 및 core/config.py 파일 작성
    - black, isort, ruff 교정 적용

---
+++ app/core/database.py
@@ -0,0 +1,9 @@
+from config import Config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
